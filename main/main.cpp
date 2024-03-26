621984e9f2a68e30a255fa17ea46a1f972a24c95
Victor Lobov <lobov@diakont.com>
2024-03-26 12:01:16 +0300

    libgit2 was added to project

--- main/main.cpp
+++ main/main.cpp
@@ -6,7 +6,4 @@ int main(int argc, char* argv[]){
     Component temp = project.get_comp();
     // temp.add_call_source("another_addr", "another", BranchType::branch, "new_branch");
     temp.print_to_screen();
-    std::cout<<temp.get_comp_list().begin()->address_in_project<<std::endl;
-    std::cout<<temp.get_comp_list().begin()->address_call_file<<std::endl;
-    std::cout<<temp.get_comp_list().begin()->branch<<std::endl;
 }
